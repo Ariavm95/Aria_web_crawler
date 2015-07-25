2cb1bf5b8e1cdca65c2caf7c528525c80980af25
Aria Vaghef <>
2015-07-24 23:05:21 -0700

    Web Crawler

---
+++ src/crawling_aria/myQuote.java
@@ -0,0 +1,27 @@
+package crawling_aria;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Aria
+ */
+public class myQuote {
+    String Author="";
+    String Quote="";
+    String Source="";
+    String Pages="";
+    public  myQuote(String a, String q, String s, String p){
+        Author=a;
+        Quote=q;
+        Source=s;
+        Pages=p;
+    }
+  
+    
+    
+}
